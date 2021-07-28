50f402ad2439222e775d852770b62a1f8ef6ed0b
lakshjadhwanilj <lakshjadhwani.lj@gmail.com>
2021-07-28 15:08:08 +0530

    Fourth Commit

--- NewJavaGitProject/src/main/java/com/lti/demo/MyFile.java
+++ NewJavaGitProject/src/main/java/com/lti/demo/MyFile.java
@@ -1,6 +1,8 @@
+package com.lti.demo;
+
 public class MyFile {
-  public static void main(String args[]){
-  System.out.println("Hello World!");
+	public static void main(String args[]) {
+		System.out.println("Hello World!");
 		System.out.println("Yes it will work");
-  }
+	}
 }
